6f4a4f85e7183f72f1a5a81d41cf3ecfa15ea08c
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-05-29 18:46:28 +0200

    added User service tests

--- src/test/java/pl/dopierala/wirepickapi/SampleUsers.java
+++ src/test/java/pl/dopierala/wirepickapi/SampleUsers.java
@@ -5,4 +5,9 @@ import pl.dopierala.wirepickapi.model.user.User;
 public class SampleUsers {
     public static User u1 = User.builder().withFirstName("user1").build();
     public static User u2 = User.builder().withFirstName("user1").build();
+
+    static{
+        u1.setId(1);
+        u2.setId(2);
+    }
 }
--- src/test/java/pl/dopierala/wirepickapi/service/UserServiceTest.java
+++ src/test/java/pl/dopierala/wirepickapi/service/UserServiceTest.java
@@ -1,16 +1,22 @@
 package pl.dopierala.wirepickapi.service;
 
 
+import javassist.tools.rmi.Sample;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import pl.dopierala.wirepickapi.SampleStock;
+import pl.dopierala.wirepickapi.SampleUsers;
 import pl.dopierala.wirepickapi.exceptions.definitions.UserLoginAlreadyExists;
 import pl.dopierala.wirepickapi.exceptions.definitions.UserNotFoundException;
 import pl.dopierala.wirepickapi.model.user.User;
 import pl.dopierala.wirepickapi.repositories.user.UserRepository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -24,7 +30,45 @@ public class UserServiceTest {
     @InjectMocks
     UserService userService;
 
-    //TODO finish user service tests
+    @Test
+    public void Should_findAllUsers_ReturnUsers(){
+        final List<User> usersSampleList = Arrays.asList(SampleUsers.u1, SampleUsers.u2);
+        when(userRepositoryMock.findAll()).thenReturn(usersSampleList);
+
+        List<User> allUsersFoundByService = userService.findAllUsers();
+
+        Assert.assertEquals(allUsersFoundByService,usersSampleList);
+    }
+
+    @Test
+    public void Should_findByLogin_ReturnUser(){
+        final User sampleUser = SampleUsers.u2;
+        when(userRepositoryMock.findByLogin(sampleUser.getLogin())).thenReturn(Optional.of(sampleUser));
+
+        User userByLogin = userService.findUserByLogin(sampleUser.getLogin());
+
+        Assert.assertEquals(userByLogin,sampleUser);
+    }
+
+    @Test
+    public void Should_findUserById_ReturnUser(){
+        final User sampleUser = SampleUsers.u2;
+        when(userRepositoryMock.findById(sampleUser.getId())).thenReturn(Optional.of(sampleUser));
+
+        User userById = userService.findUserById(sampleUser.getId());
+
+        Assert.assertEquals(userById,sampleUser);
+    }
+
+    @Test
+    public void Should_isUserExists_ReturnTrueWhenFound(){
+        final User sampleUser = SampleUsers.u2;
+        when(userRepositoryMock.findByLogin(sampleUser.getLogin())).thenReturn(Optional.of(sampleUser));
+
+        boolean userExists = userService.isUserExists(sampleUser.getLogin());
+
+        Assert.assertTrue(userExists);
+    }
 
     @Test(expected = UserNotFoundException.class)
     public void Should_findUserByLogin_ThrowException_WhenUserNotFound() {
